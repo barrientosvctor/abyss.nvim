9cbfbe8952829b9f8f91679db55ab3ccafd75ccb
barrientosvctor <vibarrientosp@gmail.com>
2024-03-30 19:00:51 -0500

    feat(setup): apply custom user options

--- lua/abyss/init.lua
+++ lua/abyss/init.lua
@@ -1,14 +1,18 @@
 local M = {}
 local util = require("abyss.util")
+local config = require("abyss.config")
 
 -- Load the theme
-function M.setup()
+--opts: The custom colorscheme user options
+---@param opts AbyssOptions
+function M.setup(opts)
 	if vim.version().minor < 8 then
 		vim.notify("Neovim +0.8 is required to use abyss.nvim colorscheme.", vim.log.levels.ERROR,
 			{ title = "Abyss.nvim" })
 		return
 	end
 
+    config.set_options(opts)
 	util.load()
 end
 
