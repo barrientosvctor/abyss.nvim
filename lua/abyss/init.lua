036841049628d3815443fd91b3c5b78632f708a6
barrientosvctor <vibarrientosp@gmail.com>
2024-04-25 16:04:24 -0500

    style: code formatting according to stylua

--- lua/abyss/init.lua
+++ lua/abyss/init.lua
@@ -1,19 +1,18 @@
 local M = {}
-local util = require("abyss.util")
 local config = require("abyss.config")
+local util = require("abyss.util")
 
 -- Load the theme
 --opts: The custom colorscheme user options
 ---@param opts AbyssOptions
 function M.setup(opts)
-    if vim.version().minor < 8 then
-        vim.notify("Neovim +0.8 is required to use abyss.nvim colorscheme.", vim.log.levels.ERROR,
-            { title = "Abyss.nvim" })
-        return
-    end
+  if vim.version().minor < 8 then
+    vim.notify("Neovim +0.8 is required to use abyss.nvim colorscheme.", vim.log.levels.ERROR, { title = "Abyss.nvim" })
+    return
+  end
 
-    config.set_options(opts)
-    util.load(config.options)
+  config.set_options(opts)
+  util.load(config.options)
 end
 
 return M
--- lua/abyss/util.lua
+++ lua/abyss/util.lua
@@ -4,17 +4,15 @@ local theme = require("abyss.theme")
 ---Performs the necessary processes to correctly load the colorscheme.
 ---@param user_opts AbyssOptions
 function util.load(user_opts)
-    vim.api.nvim_command("hi clear")
+  vim.api.nvim_command("hi clear")
 
-    if vim.fn.exists("syntax_on") then
-        vim.api.nvim_command("syntax reset")
-    end
+  if vim.fn.exists("syntax_on") then vim.api.nvim_command("syntax reset") end
 
-    vim.o.termguicolors = true
-    vim.g.colors_name = "abyss"
+  vim.o.termguicolors = true
+  vim.g.colors_name = "abyss"
 
-    theme.load_colorscheme(user_opts)
-    theme.load_terminal()
+  theme.load_colorscheme(user_opts)
+  theme.load_terminal()
 end
 
 return util
