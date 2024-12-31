a51d0cfb377920f3d9877b05faefb941114c7004
Eric Gotkowski <egotkowski@gmail.com>
2024-12-30 22:59:40 -0600

    init boreal lualine theme

---
+++ lua/lualine/themes/abyss-boreal.lua
@@ -0,0 +1,39 @@
+local colors = require("abyss.palettes.abyss_boreal")
+
+return {
+  normal = {
+    a = { fg = colors.white, bg = colors.bg_alt, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.bg_alt },
+    c = { fg = colors.fg, bg = colors.bg_alt },
+  },
+  visual = {
+    a = { fg = colors.shinyblue, bg = colors.bg_alt, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.bg_alt },
+    c = { fg = colors.fg, bg = colors.bg_alt },
+  },
+  replace = {
+    a = { fg = colors.red, bg = colors.bg_alt, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.bg_alt },
+    c = { fg = colors.fg, bg = colors.bg_alt },
+  },
+  insert = {
+    a = { fg = colors.darkgreen, bg = colors.bg_alt, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.bg_alt },
+    c = { fg = colors.fg, bg = colors.bg_alt },
+  },
+  command = {
+    a = { fg = colors.purple, bg = colors.bg_alt, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.bg_alt },
+    c = { fg = colors.fg, bg = colors.bg_alt },
+  },
+  terminal = {
+    a = { fg = colors.yellow, bg = colors.bg_alt, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.bg_alt },
+    c = { fg = colors.fg, bg = colors.bg_alt },
+  },
+  inactive = {
+    a = { fg = colors.midblue, bg = colors.bg_alt, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.bg_alt },
+    c = { fg = colors.fg, bg = colors.bg_alt },
+  },
+}
